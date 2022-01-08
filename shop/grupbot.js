f7f3f3c67f708163bf1da136a092532087b1a544
AmamiyaRen666 <65903379+AmamiyaRen666@users.noreply.github.com>
2022-01-08 16:02:37 +0700

    Update grupbot.js

--- shop/grupbot.js
+++ shop/grupbot.js
@@ -2,14 +2,14 @@
 const gcbotwa = () =>{
 	return`Join Aja Semua Fitur Bot Bisa Digunakan !
 
-1. *Alphabot Support*
-_https://chat.whatsapp.com/EU890BcXjyBDkNaUT5WmYV_
-2. *Alphabot Support 2*
-_https://chat.whatsapp.com/E8NExJwIbhBJYzssfqJNsE_
-3. *Alphabot Support 3*
-_https://chat.whatsapp.com/KCSqHTky1apG7ApePsfiPy_
-4. *Alphabot Support 4* 
-_https://chat.whatsapp.com/KwmvHr7VMFj7r5ry9xmMsU_
+1. *SusatoMikotobabot*
+_TIDAK ADA_
+2. *SusatoMikotobabot2*
+_TIDAK ADA_
+3. *SusatoMikotobabot 3*
+_TIDAK ADA_
+4. *SusatoMikotobabot 4* 
+_TIDAK ADA_
 
 Jika ada link yang ke reset, silahkan hubungi
 owner untuk meminta link yang baru
